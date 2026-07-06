930a35ad00d990b392b3fbebbecc18e1296a06b2
prajwalpatil-afk <prajwalpatil910@gmail.com>
2026-07-06 21:38:51 +0530

    default password encoder

---
+++ src/main/java/boilerplate_auth_system/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package boilerplate_auth_system.config;
+
+//@Configuration
+//public class SecurityConfig {
+//    @Bean
+//    public UserDetailsService users() {
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        UserDetails user1 = userBuilder.username("admin").password("admin").roles("ADMIN").build();
+//        return new InMemoryUserDetailsManager(user1);
+//    }
+//}
--- src/main/java/boilerplate_auth_system/entities/User.java
+++ src/main/java/boilerplate_auth_system/entities/User.java
@@ -2,11 +2,11 @@ package boilerplate_auth_system.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Getter
 @Setter
@@ -16,7 +16,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "user_id")
@@ -49,6 +49,36 @@ public class User {
     @PreUpdate
     protected void onUpdate(){
         updatedAt = Instant.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
 
