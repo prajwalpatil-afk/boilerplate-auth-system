73e58fea2befedde05a842e997df562aab0278da
prajwalpatil-afk <prajwalpatil910@gmail.com>
2026-07-06 22:33:10 +0530

    Security Config updated

--- src/main/java/boilerplate_auth_system/config/SecurityConfig.java
+++ src/main/java/boilerplate_auth_system/config/SecurityConfig.java
@@ -1,11 +1,40 @@
 package boilerplate_auth_system.config;
 
-//@Configuration
-//public class SecurityConfig {
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.
+                authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests
+                                .requestMatchers("/api/v1/auth/register").permitAll()
+                                .requestMatchers("/api/v1/auth/login").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 //    @Bean
 //    public UserDetailsService users() {
 //        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 //        UserDetails user1 = userBuilder.username("admin").password("admin").roles("ADMIN").build();
 //        return new InMemoryUserDetailsManager(user1);
 //    }
-//}
+}
--- src/main/java/boilerplate_auth_system/services/impl/AuthServiceImpl.java
+++ src/main/java/boilerplate_auth_system/services/impl/AuthServiceImpl.java
@@ -4,14 +4,17 @@ import boilerplate_auth_system.dtos.UserDto;
 import boilerplate_auth_system.services.AuthService;
 import boilerplate_auth_system.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
+    private final PasswordEncoder passwordEncoder;
     private final UserService userService;
     @Override
     public UserDto registerUser(UserDto userDto) {
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         UserDto userDto1 = userService.createUser(userDto);
         return userDto1;
     }
