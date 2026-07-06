1dc3a36cdce692f54a8ce35745fb00f8d1685cc1
prajwalpatil-afk <prajwalpatil910@gmail.com>
2026-07-06 21:50:30 +0530

    implementing UserDetails

--- src/main/java/boilerplate_auth_system/entities/User.java
+++ src/main/java/boilerplate_auth_system/entities/User.java
@@ -3,6 +3,7 @@ package boilerplate_auth_system.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
@@ -53,32 +54,38 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        List<SimpleGrantedAuthority> authorities = roles
+                .stream()
+                .map(role -> {
+                    return new SimpleGrantedAuthority(role.getName());
+                })
+                .toList();
+        return authorities;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return this.email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return this.enable;
     }
 }
 
