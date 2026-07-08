809a7b52f0bb69f4a93be7a660f2e2c9a52e7ea3
prajwalpatil-afk <prajwalpatil910@gmail.com>
2026-07-08 17:45:30 +0530

    updated security config

--- src/main/java/boilerplate_auth_system/config/SecurityConfig.java
+++ src/main/java/boilerplate_auth_system/config/SecurityConfig.java
@@ -1,29 +1,52 @@
 package boilerplate_auth_system.config;
 
 
+import boilerplate_auth_system.security.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.
-                authorizeHttpRequests(authorizeHttpRequests ->
+        http.csrf(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeHttpRequests ->
                         authorizeHttpRequests
                                 .requestMatchers("/api/v1/auth/register").permitAll()
                                 .requestMatchers("/api/v1/auth/login").permitAll()
-                                .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, e) -> {
+                    //error message
+                    e.printStackTrace();
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setContentType("application/json");
+                    String message = "Unauthorized Access: " + e.getMessage();
+                    Map<String,String> errorMap = Map.of("message", message,"statusCode", String.valueOf(HttpServletResponse.SC_UNAUTHORIZED));
+                    var objectMapper = new ObjectMapper();
+                    response.getWriter().write(objectMapper.writeValueAsString(errorMap));
+                }))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -38,3 +61,5 @@ public class SecurityConfig {
 //        return new InMemoryUserDetailsManager(user1);
 //    }
 }
+
+
