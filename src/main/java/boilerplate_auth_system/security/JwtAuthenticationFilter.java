f1ca4eade651cbfa918b4e0d14ff7e4fc156c5f0
prajwalpatil-afk <prajwalpatil910@gmail.com>
2026-07-08 16:01:21 +0530

    JwtAuthenticationFilter updated

--- src/main/java/boilerplate_auth_system/security/JwtAuthenticationFilter.java
+++ src/main/java/boilerplate_auth_system/security/JwtAuthenticationFilter.java
@@ -1,4 +1,105 @@
 package boilerplate_auth_system.security;
 
-public class JwtAuthenticationFilter {
+import boilerplate_auth_system.entities.Role;
+import boilerplate_auth_system.repositories.UserRepository;
+import boilerplate_auth_system.utility.UserUtility;
+import io.jsonwebtoken.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+
+            String token = header.substring(7);
+
+            // Process only access tokens
+            if (!jwtService.isAccessToken(token)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            try {
+
+                Jws<Claims> parse = jwtService.parse(token);
+                Claims payload = parse.getPayload();
+
+                String userId = payload.getSubject();
+                UUID userUuid = UserUtility.parseUUID(userId);
+
+                userRepository.findById(userUuid).ifPresent(user -> {
+
+                    // Check if user is enabled
+                    if (user.isEnabled()) {
+
+                        List<GrantedAuthority> authorities = user.getRoles() == null
+                                ? List.of()
+                                : user.getRoles()
+                                .stream()
+                                .map(Role::getName)
+                                .map(SimpleGrantedAuthority::new)
+                                .map(GrantedAuthority.class::cast)
+                                .toList();
+
+                        UsernamePasswordAuthenticationToken authentication =
+                                new UsernamePasswordAuthenticationToken(
+                                        user.getEmail(),
+                                        null,
+                                        authorities
+                                );
+
+                        authentication.setDetails(
+                                new WebAuthenticationDetailsSource()
+                                        .buildDetails(request)
+                        );
+
+                        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                            SecurityContextHolder.getContext().setAuthentication(authentication);
+                        }
+                    }
+                });
+
+            } catch (ExpiredJwtException e) {
+                e.printStackTrace();
+
+            } catch (MalformedJwtException e) {
+                e.printStackTrace();
+
+            } catch (JwtException e) {
+                e.printStackTrace();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
